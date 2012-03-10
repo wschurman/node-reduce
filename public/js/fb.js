9329827291fc51c0a92f02ab37ad1b7cd447a924
jfeinstein10 <j.feinstein10@gmail.com>
2012-03-10 07:11:29 -0500

    Replaced post-initialize checking.

--- public/js/fb.js
+++ public/js/fb.js
@@ -5,6 +5,19 @@ FB.init({
     xfbml: true
 });
 
+$(function() {
+    FB.getLoginStatus(function(response) {
+        if (response.status == 'connected' || response.status == 'not_authorized') {
+            $('fbLogin').hide();
+            $('fbLogout').show();
+            $('shareList').show();
+        } else {
+            $('fbLogin').show();
+            $('fbLogout').hide();
+            $('shareList').hide();
+        }
+    });
+});
 
 function login() {
     FB.login(function(response) {
