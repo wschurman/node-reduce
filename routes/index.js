246e35143be8a0e6653b33c215e2bced8fc4e258
wschurman <wschurman@gmail.com>
2012-03-10 14:23:49 -0500

    hahaha

--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,7 @@ exports.index = function(req, res){
 exports.client = function(req, res){
 	res.render('client', { title: 'I am a worker' });
 }
+
+exports.about = function(req, res) {
+	res.render('about', {title: "About Node Reduce"});
+}
